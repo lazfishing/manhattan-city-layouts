66820cb52e27ae20324123214d56b0d6a8d3d2a6
lazfishing <54772874+lazfishing@users.noreply.github.com>
2021-07-19 14:31:57 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,7 @@ import random
 import time
 import os
 import SessionState
+import inference
 
 def get_session_state(rando):
     session_state = SessionState.get(random_number=random.random(), nsamples='', 
@@ -31,6 +32,32 @@ def display_app_header(main_txt,sub_txt,is_sidebar = False):
         
 def display_side_panel_header(txt):
     st.sidebar.markdown(f'## {txt}')
+    
+def latent_interpolation(nta_A,nta_B):
+    manhattan_clusters.nta.unique()
+    A = int(np.linspace(1,50)[[i for i, val in enumerate(clusters.nta.unique()==nta_A) if val][0]])
+    B = int(np.linspace(1,50)[[i for i, val in enumerate(clusters.nta.unique()==nta_B) if val][0]])
+    
+    latent0 = root_F_sample[A]
+    latent1 = root_F_sample[B]
+
+    steps = [latent0]+[latent0 + x*(latent1-latent0) for x in torch.linspace(0.1, 0.8, steps=10)]+[latent1]
+    latent_steps = torch.stack(steps, dim=0)
+
+    P0 = torch.reshape(root_P_list[A],(1,5))
+    P1 = torch.reshape(root_P_list[B],(1,5))
+
+    p_steps = [P0]+[P0 + x*(P1-P0) for x in torch.linspace(0.1, 0.8, steps=10)]+[P1]
+    P_steps = torch.stack(p_steps, dim=0)
+
+    # root_P = torch.Tensor([[0.4, 0.6, 0.8, 0.9, -0.4]])
+    Box_Set_List = []
+    for i in range(12):
+        P_list, I_list, Set_list = inference_final_set(P_steps[i],latent_steps[i:i+1], 20)
+        Set_list = Set_list.detach().numpy()
+        box_set = get_box_2(Set_list[:,:2],Set_list[:,2:])
+        Box_Set_List.append(box_set)
+    plot_boxes2(Box_Set_List,2,6)
 
 def main():
     st.set_page_config(page_title='Manhattan City Layout Analysis') #layout='wide', initial_sidebar_state='auto'
@@ -62,7 +89,8 @@ def main():
     ### Blending City Layouts ###
     if session_state.pages == 'Blending City Layouts':
         st.text("Blending City Layouts")
-        st.selectbox('Neighbourhood A', options=manhattan_clusters.nta.unique()[:30])
+        nta_A = st.selectbox('Neighbourhood A', options=manhattan_clusters.nta.unique()[:29])
+        nta_B = st.selectbox('Neighbourhood B', options=manhattan_clusters.nta.unique()[:29])
         
 if __name__ == "__main__":
     main()
