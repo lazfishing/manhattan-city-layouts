195c8fd43329cef1aa1f643bb5dffcdebee12291
lazfishing <54772874+lazfishing@users.noreply.github.com>
2021-07-21 20:11:26 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -84,7 +84,7 @@ def main():
                     pickable=True,
                 ),
             ],
-            tooltip={"html": "{ntaname}: {gmm_pca.toFixed(2)}", "style": {"backgroundColor": "black", "color": "white", "font-size": "12px"} },
+            tooltip={"html": "{ntaname}: {gmm_pca}", "style": {"backgroundColor": "black", "color": "white", "font-size": "12px"} },
         ))
 
     ### Blending City Layouts ###
