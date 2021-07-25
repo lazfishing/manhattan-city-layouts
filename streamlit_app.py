ba97d4efb8e3a223117f6bfe1fe98ee5c7e0d127
lazfishing <54772874+lazfishing@users.noreply.github.com>
2021-07-25 17:47:28 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -76,7 +76,7 @@ def main():
             opacity=0.6,
             stroked=True,
             filled=True,
-            get_fill_color='[deviation * 0.9, deviation * 0.9, 255]',
+            get_fill_color='[deviation * 255, deviation * 255, 255]',
             auto_highlight=True,
             pickable=True)
         
