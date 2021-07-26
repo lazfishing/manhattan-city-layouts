3e8b2d383f495708e411c9a4f00266b23ea06fb2
lazfishing <54772874+lazfishing@users.noreply.github.com>
2021-07-25 21:07:50 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -112,7 +112,7 @@ def main():
         st.pydeck_chart(pdk.Deck(
             map_style='mapbox://styles/mapbox/light-v9',
             initial_view_state=pdk.ViewState(
-                latitude=40.7801,
+                latitude=40.7751,
                 longitude=-73.9812,
                 zoom=10.2,
                 pitch=35,
