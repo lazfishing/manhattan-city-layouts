fa20e240483ef3fdb83df3d2fc903b03f3c20b81
lazfishing <54772874+lazfishing@users.noreply.github.com>
2021-07-27 18:33:23 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,6 +53,15 @@ def main():
         sub_txt = "2021 NYU CUSP Capstone Project"
         display_app_header(main_txt,sub_txt,is_sidebar = False)
         
+        st.write("City layouts - or urban parcellations - are incredibly diverse and complex. Simple metrics like building footprint area \
+                are incomplete in describing the higher-dimensional complexity and spatial hierarchy of these layouts. Other design language, \
+                like 'urban grain' or 'morphology', are qualitative. Consensus on describing city layouts or classifying them into meaningful \
+                urban typologies is hard to come by.")
+
+        st.write("This project is an attempt to describe city layouts in a standardized, objective way by encoding their spatial structure using \
+                deep neural networks. We show how their encoded latent features can be used in downstream urban planning applications, including \
+                neigborhood analysis and generating composite city layouts.")
+        
     ### Clustering City Layouts ###
     if session_state.pages == 'Clustering City Layouts':
         sub_txt = "Clustering City Layouts"
