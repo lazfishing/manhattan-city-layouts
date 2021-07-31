0c6b96f5efc9a3d32d9934536132c2e9904fa6b8
lazfishing <54772874+lazfishing@users.noreply.github.com>
2021-07-31 00:51:15 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -101,7 +101,7 @@ def main():
             opacity=0.6,
             stroked=True,
             filled=True,
-            get_fill_color='[255-gmm_pca_color, 255-gmm_pca_color, 255-gmm_pca_color]',
+            get_fill_color='[255-gmm_pca_color, gmm_pca_color, 255-gmm_pca_color]',
             auto_highlight=True,
             pickable=True)
         
